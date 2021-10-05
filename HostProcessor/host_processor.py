fd9f9202b5c57e9f99163e5196c60836b3558621
EC2 Default User <ec2-user@ip-10-101-0-36.ec2.internal>
2021-10-05 11:43:32 +0000

    added retry, added XFF, Single ip per row

--- HostProcessor/host_processor.py
+++ HostProcessor/host_processor.py
@@ -14,6 +14,8 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.setLevel(int(os.getenv("LOG_LVL", '10')))
 
+WAF_BLOCK_IP_TABLE = os.environ['BLOCKLIST_DYNAMODB']
+
 TIMESTREAM_DB_NAME = os.environ["TIMESTREAM_DB_NAME"]
 TIMESTREAM_TABLE_NAME = os.environ["TIMESTREAM_TABLE_NAME"]
 
@@ -37,49 +39,73 @@ class IpDetails(TypedDict):
 # WAF V2 CloudFront is only supported in Region 'us-east-1' and Thus, cannot be changed
 waf_client = boto3.client('wafv2', region_name='us-east-1')
 
-def save_block_history(host: str, distribution: str, blocked_ip: List[str]) -> None:
+def remove_unwanted_ip(ips: List[str]) -> List[str]:
+    try:
+        ips.remove('10.0.0.0/32')
+        return ips
+    except ValueError as e:
+        logger.debug(e)
+    try:
+        ips.remove('fd4b:9821:be17:8c1e:0000:0000:0000:0000/128')
+        return ips
+    except ValueError as r:
+        logger.debug(r)
+
+def save_block_history(host: str, distribution: str, blocked_ips: List[str], ip_type: str, ipset_id: str) -> None:
     """Inserts Blocked Ip data in dynamodb table"""
 
+    remove_unwanted_ip(blocked_ips)
+
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['BLOCKLIST_DYNAMODB'])
+    table = dynamodb.Table(WAF_BLOCK_IP_TABLE)
     # with put_item function we insert data in Table
-    table.put_item(
-        Item={
-            'host': host,
-            'distribution': distribution,
-            'time': str(datetime.datetime.now()),
-            'ip': blocked_ip
-        }
-    )
-    logger.debug("Added %s ip in Dynamo DB table %s", blocked_ip, os.environ['BLOCKLIST_DYNAMODB'])
+
+    with table.batch_writer(overwrite_by_pkeys=["distribution", "host"]) as batch:
+        for ip in blocked_ips:
+            values = {
+                    'host': host,
+                    'distribution': distribution,
+                    'time': str(datetime.datetime.now()),
+                    'ip': ip,
+                    'ip_type': ip_type,
+                    'ipset_id': ipset_id,
+                }
+            logger.debug(f"Dynamodb table {WAF_BLOCK_IP_TABLE} data: {values}")
+            batch.put_item(
+                Item=values
+            )
+    logger.debug("Added %s ip in Dynamo DB table %s", blocked_ips, WAF_BLOCK_IP_TABLE)
 
 
-def update_waf_ipset(ipset_name: str, ipset_id: str, ips_to_be_blocked: Set[str], host: str, distribution: str, ip_type: str) -> List[str]:
+def update_waf_ipset(ipset_name: str, ipset_id: str, ips_to_be_blocked: Set[str], host: str, distribution: str, ip_type: str, retry: int =3) -> List[str]:
     """Add offending ips in waf ip_set"""
 
     if ip_type == "IPv4":
         ips_to_be_blocked = set(map(lambda x: x+"/32", ips_to_be_blocked))
     else:
         ips_to_be_blocked = set(map(lambda x: x+"/128", ips_to_be_blocked))
-    logger.debug("Blocking: %s", str(ips_to_be_blocked))
 
     # Get LockToken and Existing Address List
     lock_token, existing_ips = get_ipset_lock_token(waf_client, ipset_name, ipset_id)
 
-    # existing_ips.extend(ips_to_be_blocked)
     ips_to_be_blocked.update(existing_ips)  # Removing duplicates
     ips_to_be_blocked_list: List[str] = list(ips_to_be_blocked)
-    # existing_ips = list(set(existing_ips)) 
+    logger.debug("Blocking: %s", str(ips_to_be_blocked_list))
 
-    waf_client.update_ip_set(
-        Name=ipset_name,
-        Scope='CLOUDFRONT',
-        Id=ipset_id,
-        Addresses=ips_to_be_blocked_list,
-        LockToken=lock_token
-    )
+    try:
+        waf_client.update_ip_set(
+            Name=ipset_name,
+            Scope='CLOUDFRONT',
+            Id=ipset_id,
+            Addresses=ips_to_be_blocked_list,
+            LockToken=lock_token
+        )
+    except waf_client.exceptions.WAFOptimisticLockException as e:
+        logger.error(e)
+        if retry > 0:
+            return update_waf_ipset(ipset_name, ipset_id, ips_to_be_blocked, host, distribution, ip_type, retry = retry - 1)
 
-    save_block_history(host, distribution, ips_to_be_blocked_list)
+    save_block_history(host, distribution, ips_to_be_blocked_list, ip_type, ipset_id)
     logger.debug("Added Offending Ip Addresses to WAF IPSets")
     return ips_to_be_blocked_list
 
@@ -102,16 +128,16 @@ def process_row(column_info: List[str], row: Dict[str, Any], threshold: int) -> 
 
     if int(row_dict["Frequency"]) > threshold:
         logger.debug("Offending Ip %s found in row %s", row_dict["c_ip"], str(row_dict))
-        return row_dict["c_ip"], row_dict["c_ip_version"]
+        return row_dict.get("c_ip") or row_dict["x_forwarded_for"], row_dict["c_ip_version"]
     return None, None
 
-def process_host(host: str, duration: str, threshold: int, offending_ips: IpDetails) -> IpDetails:
+def process_host(header: str, host: str, duration: str, threshold: int, offending_ips: IpDetails) -> IpDetails:
     """Query Timestream db for logs and return ips where count greater than threshold"""
 
     query_client = boto3.client('timestream-query')
     paginator = query_client.get_paginator('query')
 
-    query = f'''SELECT c_ip, cs_host, cs_uri_stem, x_host_header, c_ip_version, COUNT(c_ip) AS Frequency FROM "{TIMESTREAM_DB_NAME}"."{TIMESTREAM_TABLE_NAME}"  WHERE cs_host='{host}' AND time between ago({duration}) and now() GROUP BY c_ip,cs_host,cs_uri_stem,c_ip_version,x_host_header ORDER BY Frequency DESC '''
+    query = f'''SELECT {header}, cs_host, cs_uri_stem, x_host_header, c_ip_version, COUNT({header}) AS Frequency FROM "{TIMESTREAM_DB_NAME}"."{TIMESTREAM_TABLE_NAME}"  WHERE cs_host='{host}' AND time between ago({duration}) and now() AND {header} != '-' GROUP BY {header},cs_host,cs_uri_stem,c_ip_version,x_host_header ORDER BY Frequency DESC '''
     logger.info("Query String = %s", query)
 
     try:
@@ -144,7 +170,7 @@ def get_config(host: str, distribution: str) -> Tuple[str, int]:
             "host": host
         }
     )
-    logger.debug("Get Configurations details from Dynamo db Successfully")
+    logger.debug(f"Configurations details from Dynamo db: {response}")
     return response['Item']['duration'], int(response['Item']['threshold'])
 
 
@@ -171,13 +197,23 @@ def lambda_handler(event: str, context) -> None:
     duration, threshold = get_config(host, distribution)
     logger.info(f"Host: {host}, distribution: {distribution}, duration: {duration}, threshold: {threshold}, from table {os.environ['CONFIGURATION_DYNAMODB']}")
 
-    offending_ips = process_host(host, duration, threshold, offending_ips_info)
-    logger.info(offending_ips)
+    headers = ("x_forwarded_for", "c_ip")
+    for header in headers:
+        offending_ips = process_host(header, host, duration, threshold, offending_ips_info)
+        logger.info(offending_ips)
 
     for ip_version, ip_details in offending_ips.items():
         if ip_details["ips"]:
             blocked_ips = update_waf_ipset(ip_details["ipset_name"], ip_details["ipset_id"], ip_details["ips"], host, distribution, ip_version)
             logger.info('Blocked Ips: %s', str(blocked_ips))
-            logger.info('Updated IPSet %s with %d CIDRs', ip_details["ipset_name"], len(blocked_ips))
+            logger.info("Updated IPSet %s with %d IP's", ip_details["ipset_name"], len(blocked_ips))
         else:
             logger.info("No %s Addresses found", ip_version)
+
+"""
+Query for x_forwarded_for column
+SELECT x_forwarded_for, cs_host, cs_uri_stem, x_host_header, c_ip_version, COUNT(x_forwarded_for) AS Frequency FROM "CloudFrontLogsTimeSeriesDb-iV8XeSLItCX2"."RealtimeLogsTable-CfP66didm9mb"  WHERE cs_host='waftest1.ccrt.us' AND time between ago(2h) and now() AND x_forwarded_for != '-' GROUP BY x_forwarded_for, cs_host,cs_uri_stem,c_ip_version,x_host_header ORDER BY Frequency DESC
+
+Query for c_ip column , same as above
+SELECT c_ip, cs_host, cs_uri_stem, x_host_header, c_ip_version, COUNT(c_ip) AS Frequency FROM "CloudFrontLogsTimeSeriesDb-iV8XeSLItCX2"."RealtimeLogsTable-CfP66didm9mb"  WHERE cs_host='waftest1.ccrt.us' AND time between ago(2h) and now() AND c_ip != '-' GROUP BY c_ip,cs_host,cs_uri_stem,c_ip_version,x_host_header ORDER BY Frequency DESC
+"""
